7121fdbdd3b45fddc29d60530248fc7162923423
Irakli Gozalishvili <contact@gozala.io>
2020-04-08 15:26:55 -0700

    fix typo in sql query

--- base/sql/insert_tag.sql
+++ base/sql/insert_tag.sql
@@ -1,5 +1,5 @@
-INSERT OR IGNORE INTO
+INSERT OR REPLACE INTO
   tags
-  (target_url, target_fragment, target_location, name);
+  (target_url, target_fragment, target_location, name)
 VALUES
   (:target_url, :target_fragment, :target_location, :name);
